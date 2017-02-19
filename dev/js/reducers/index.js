dab3a5f1a5af7c32946a45b72b526fc619bfeb6c
sunnyworld2009 <sunnygiri963@gmail.com>
2017-02-20 01:16:02 +0530

    Remove already madeup files and started develoment from scratch

--- dev/js/reducers/index.js
+++ dev/js/reducers/index.js
@@ -1,15 +1,6 @@
 import {combineReducers} from 'redux';
-import UserReducer from './reducer-users';
-import ActiveUserReducer from './reducer-active-user';
-
-/*
- * We combine all reducers into a single object before updated data is dispatched (sent) to store
- * Your entire applications state (store) is just whatever gets returned from all your reducers
- * */
+import userReducer from './reducer-users';
 
 const allReducers = combineReducers({
-    users: UserReducer,
-    activeUser: ActiveUserReducer
+  users: userReducer
 });
-
-export default allReducers
