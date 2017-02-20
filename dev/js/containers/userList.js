b2942c71e3dc3b236fc996eb057c98b6c49ba8fd
sunnyworld2009 <sunnygiri963@gmail.com>
2017-02-20 22:39:34 +0530

    Working my way towards redux, getting a little grab over things, now makes a little sense why I was doing all those things before unknowingly haha

--- dev/js/containers/userList.js
+++ dev/js/containers/userList.js
@@ -1,21 +1,28 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {selectUser} from '../actions';
 
 class UserList extends Component {
+  _renderList() {
+    return this.props.users.map((user ,index) => {
+        return (
+          <li key={user.id}>{user.first}</li>
+        );
+    });
+  }
   render() {
     return (
       <ul>
-        <li>One</li>
-        <li>One</li>
-        <li>One</li>
-        <li>One</li>
+        {
+          this._renderList()
+        }
       </ul>
   )
  }
 }
 
-function mapStateToProps() {
+function mapStateToProps(state) {
   return {
     users: state.users
   }
